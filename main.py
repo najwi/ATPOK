026be45d7d3ed146f2e60b1badb53941787be5b3
Michal Najwer <254560@student.pwr.edu.pl>
2021-04-15 23:23:10 +0200

    Gui1

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+CURRENCIES = ["BTC", "ETH", "XRP", "DASH", "DOGE"]
+BUTTON_COLOR_CLICKED = (232 / 255, 159 / 255, 0, 1)
+BUTTON_COLOR_NOT_CLICKED = (130 / 255, 130 / 255, 130 / 255, 1)
 
 
-# Press the green button in the gutter to run the script.
+class MyButton(Button):
+
+    def __init__(self, **kwargs):
+        super(MyButton, self).__init__(**kwargs)
+        self.pressed = False
+
+
+class RootWidget(FloatLayout):
+
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+
+        for b in CURRENCIES:
+            self.ids.buttons_grid.add_widget(MyButton(
+                text=b,
+                background_normal="",
+                background_color=BUTTON_COLOR_NOT_CLICKED,
+                background_down="",
+                on_press=currency_button_press,
+                size_hint_max_y=50
+            ))
+
+
+def currency_button_press(instance):
+    if instance.pressed:
+        instance.background_color = BUTTON_COLOR_NOT_CLICKED
+        instance.pressed = False
+    else:
+        instance.background_color = BUTTON_COLOR_CLICKED
+        instance.pressed = True
+
+
+class AtpokApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    AtpokApp().run()
